2afc154de67b79c1687c3388ef7090174c4811db
japar <japar@192.168.1.66>
2023-04-27 11:46:01 +0200

    NurseManager created

---
+++ src/Hospital_inventoryInterfaces/NurseManager.java
@@ -0,0 +1,15 @@
+package Hospital_inventoryInterfaces;
+
+import java.util.List;
+
+import Hospital_InventoryPOJO.Nurse;
+
+public interface NurseManager {
+
+	//add a administrator
+		public void addNurse(Nurse n);
+		//get a list of the administrators
+		public List<Nurse> getListNurse();
+		//remove an administrator by it´s id
+		public void removeNurse(int id);
+}
