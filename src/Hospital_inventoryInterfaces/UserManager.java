97bd7d5a3693a9409d648eae52459f3f5e08c06c
Andreoti <Andreoti@DESKTOP-M0Q8C79>
2023-04-28 09:55:07 +0200

    UserManagerInterface created

---
+++ src/Hospital_inventoryInterfaces/UserManager.java
@@ -0,0 +1,21 @@
+package Hospital_inventoryInterfaces;
+
+import java.util.List;
+
+import Hospital_InventoryPOJO.Role;
+import Hospital_InventoryPOJO.User;
+
+public interface UserManager {
+
+	public void connect();
+	public void disconnect();
+	public void newUser(User u);
+	public void newRole(Role r);
+	
+	public Role getRole(String email);
+	
+	public List<Role> getRoles();
+	
+	public User checkPassword(String email, String passwd);
+}
+
